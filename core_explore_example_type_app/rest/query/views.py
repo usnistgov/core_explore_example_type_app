1cad8cadec398672786c1e8c797fea28e3278056
Adrien Catel <adrien.catel@nist.gov>
2017-09-26 15:58:19 -0400

    fix/ remove unneeded sub document root
    Change-Id: I3501a9d439e0ffadf183275c11d3fb965ba2fb5b

--- core_explore_example_type_app/rest/query/views.py
+++ core_explore_example_type_app/rest/query/views.py
@@ -5,7 +5,7 @@ from core_explore_example_type_app.components.data_item import api as data_item_
 
 
 class ExecuteLocalQueryDataItemView(ExecuteLocalQueryView):
-    sub_document_root = 'list_content'
+    sub_document_root = None
 
     def execute_raw_query(self, raw_query):
         return data_item_api.execute_query_distinct_by_data(raw_query, self.request.user)
