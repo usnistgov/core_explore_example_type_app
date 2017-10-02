85bb9fa8f4e082e39efdd3e07ac90b954c89e7aa
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2017-10-02 11:29:39 -0400

    Update version: alpha2
    Change-Id: I888584822146ca5b71b0ce99272ed8e9b931a2aa

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="core_explore_example_type_app",
-    version="1.0.0-alpha1",
+    version="1.0.0-alpha2",
     description=("core explore query by example (for Types) Django package",),
     author="NIST IT Lab",
     author_email="itl_inquiries@nist.gov",
