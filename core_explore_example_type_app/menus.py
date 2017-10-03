210427dad97daf4a5f09407addb568641f2a188a
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2017-10-03 14:15:43 -0400

    Put back the menu
    Change-Id: I5f08365217cbbdf85e3b30c28d0b6c3001ce1dc8

--- core_explore_example_type_app/menus.py
+++ core_explore_example_type_app/menus.py
@@ -5,7 +5,6 @@ from django.core.urlresolvers import reverse
 from menu import Menu, MenuItem
 
 # FIXME: CHECK AUTHENTICATION !
-# FIXME: PUT BACK THE MENU ONCE THE EXPLORE BY TYPE WORKS !
-# Menu.add_item(
-#     "main", MenuItem("Query by Type", reverse("core_explore_example_type_index"))
-# )
+Menu.add_item(
+    "main", MenuItem("Query by Type", reverse("core_explore_example_type_index"))
+)
